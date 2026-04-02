1f5c02bb17583304b8bf3112a9bb937085570636
Sebastien Levy <m374-crypt0@pm.me>
2026-04-02 08:56:54 +0000

    feat(template): list

---
+++ src/template_list_command.sh
@@ -0,0 +1,5 @@
+echo "# This file is located at 'src/template_list_command.sh'."
+echo "# It contains the implementation for the 'tide template list' command."
+echo "# The code you write here will be wrapped by a function named 'tide_template_list_command()'."
+echo "# Feel free to edit this file; your changes will persist when regenerating."
+inspect_args
