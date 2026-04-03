c1bd912e663db2b34a08861c3267f973b61d744c
Sebastien Levy <m374-crypt0@pm.me>
2026-04-03 17:59:58 +0000

    docs(draft): run script

--- draft/protobase/run.sh
+++ draft/protobase/run.sh
@@ -1,3 +1,5 @@
+# WARN: can only work in latest version of nideovim because of
+# DOCKER_SOCKET_PATH variable uses
 docker run --rm -it \
   -e DOCKER_HOST_SOCKET_PATH="$DOCKER_SOCKET_PATH" \
   -v "$DOCKER_SOCKET_PATH":/var/run/docker.sock \
