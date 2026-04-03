1e3dfa8bd6f7545c449322e8ed6e7a7a9bf18bcf
Sebastien Levy <m374-crypt0@pm.me>
2026-04-03 17:04:21 +0000

    test(draft): running

--- draft/alpine/docker/run.sh
+++ draft/alpine/docker/run.sh
@@ -1,1 +1,4 @@
-docker run --rm -it -v "$DOCKER_SOCKET_PATH":/var/run/docker.sock test bash
+docker run --rm -it \
+  -e DOCKER_HOST_SOCKET_PATH="$DOCKER_SOCKET_PATH" \
+  -v "$DOCKER_SOCKET_PATH":/var/run/docker.sock \
+  test bash
