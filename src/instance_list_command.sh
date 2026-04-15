ae39645f8bd66cc992c403fa82636625513f3b50
Sebastien Levy <m374-crypt0@pm.me>
2026-04-15 13:45:33 +0000

    feat(instance list): first test

--- src/instance_list_command.sh
+++ src/instance_list_command.sh
@@ -1,5 +1,8 @@
-echo "# This file is located at 'src/instance_list_command.sh'."
-echo "# It contains the implementation for the 'tide instance list' command."
-echo "# The code you write here will be wrapped by a function named 'tide_instance_list_command()'."
-echo "# Feel free to edit this file; your changes will persist when regenerating."
-inspect_args
+# echo "# This file is located at 'src/instance_list_command.sh'."
+# echo "# It contains the implementation for the 'tide instance list' command."
+# echo "# The code you write here will be wrapped by a function named 'tide_instance_list_command()'."
+# echo "# Feel free to edit this file; your changes will persist when regenerating."
+# inspect_args
+
+echo You are not in a tide project >&2
+exit 1
--- test/suites/instance/list.bats
+++ test/suites/instance/list.bats
@@ -9,6 +9,11 @@ setup() {
 
   load "${TIDE_ROOT_DIR}test/test_helper/tide_helpers.sh"
   load "${TIDE_ROOT_DIR}test/test_helper/tide_assert.sh"
+
+  # shellcheck disable=SC2329
+  instance_list_command() {
+    load "${TIDE_ROOT_DIR}src/instance_list_command.sh"
+  }
 }
 
 teardown() {
@@ -16,5 +21,8 @@ teardown() {
 }
 
 @test 'Listing instances outside of a project fails' {
-  skip
+  run instance_list_command
+
+  assert_failure
+  assert_output 'You are not in a tide project'
 }
