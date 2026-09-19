3bb37a6b6a1cda96c9d586e01d9160c87f1ec6d4
crypt0 <m374-crypt0@pm.me>
2026-09-19 11:14:23 +0200

    test(draft): llvm prototype scaffolding

---
+++ draft/llvm-prototype/02-build-llvm-builder.sh
@@ -0,0 +1,14 @@
+#!/bin/env bash
+
+base_stamp=edge
+this_stamp=$(date -u +%4Y%m%d%H%M%S)
+
+docker build \
+  -f docker/tide-llvm-prototype.Dockerfile \
+  --build-arg BASE_STAMP=$base_stamp \
+  --build-arg USER_UID="$(id -u)" \
+  --build-arg USER_GID="$(id -g)" \
+  --build-arg USER_NAME="$(id -nu)" \
+  -t m374crypt0/tide-llvm-prototype:edge \
+  -t m374crypt0/tide-llvm-prototype:"$this_stamp" \
+  ./docker
---
+++ draft/llvm-prototype/docker/tide-llvm.Dockerfile
@@ -0,0 +1,3 @@
+ARG BASE_STAMP=edge
+
+FROM m374crypt0/tide-alpine:${BASE_STAMP}
